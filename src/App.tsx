7b70d6725e50de4512071c95ab9c9e320fca5b6f
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-10-17 13:51:04 +0800

    Login Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import Index from './pages/Signup/index'
-import Home from './pages/Home'
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
-import ProtectedRoute from './components/ProtectedRoute';
-import PublicRoute from './components/PublicRoute';
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login";
 function App() {
   return (
-      <Routes>
-        <Route path='/signup/login' element={<PublicRoute><Index/></PublicRoute>}/>
-        <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
